e9707a7a66a40a343d9baac31c917fead1dfd544
Caroline Caruana <Caroline Caruana>
2016-04-05 20:04:48 +0200

    Created Genre class

--- src/main/java/sengassignment/Library/Genre.java
+++ src/main/java/sengassignment/Library/Genre.java
@@ -5,10 +5,19 @@
  */
 package sengassignment.Library;
 
-/**
- *
- * @author Caroline
- */
 public class Genre {
+
+    private String type;
     
+    public Genre(){
+        type = "";
+    }
+    
+    public Genre(String type){
+        this.type = type;
+    }
+    
+    public String getType(){
+        return this.type;
+    }
 }
