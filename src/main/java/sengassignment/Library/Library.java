5cdae0db1adb2ad3c9889a9bd75a67738d2477e4
Caroline Caruana <Caroline Caruana>
2016-04-05 20:51:35 +0200

    Created Library class

--- src/main/java/sengassignment/Library/Library.java
+++ src/main/java/sengassignment/Library/Library.java
@@ -1,14 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sengassignment.Library;
 
-/**
- *
- * @author Caroline
- */
 public class Library {
+    
+    public Library(){
+        
+    }
+    
+    public void addUser(User u){
+        
+    }
+    
+    public void removeUser(User u){
+        
+    }
+    
+    public void loanBookTo(Book b, User u){
+        
+    }
+    
+    public void returnBook(Book b){
+        
+    }
     
 }
