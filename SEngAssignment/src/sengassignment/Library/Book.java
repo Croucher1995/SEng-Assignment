42b07aa12d1c3fe4c2eca5d9732de0bfa3b3da27
Caroline Caruana <Caroline Caruana>
2016-04-05 10:25:42 +0200

    Created constructors and getters

--- SEngAssignment/src/sengassignment/Library/Book.java
+++ SEngAssignment/src/sengassignment/Library/Book.java
@@ -1,8 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+package sengassignment.Library;
+
+import java.io.*;
+
 package sengassignment.Library;
 
 /**
@@ -10,5 +9,7 @@ package sengassignment.Library;
  * @author Caroline
  */
 public class Book {
-    
+    public Book(){
+        string 
+    }
 }
