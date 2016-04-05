9c342f8d7610f0c63af531216f0156ca884e5fe1
Andre Croucher <andrecroucher@gmail.com>
2016-04-05 18:52:18 +0200

    Created User class

--- src/main/java/sengassignment/Library/User.java
+++ src/main/java/sengassignment/Library/User.java
@@ -5,10 +5,56 @@
  */
 package sengassignment.Library;
 
+import java.io.*;
+import java.util.UUID;
+
 /**
  *
  * @author Caroline
  */
 public class User {
-    
+    private String name;
+    private String surname;
+    private String town;
+    private String email_address;
+    private UUID ID;
+  
 }
+
+//to initialise
+    public User(){
+        name = "";
+        surname = "";
+        town = "";
+        email_address = "";
+    }
+
+    public Book(String name, String surname, String town, String email_address){
+        this.name = name;
+        this.surname = surname;
+        this.town = town;
+        this.email_address = email_address;
+        this.ID = UUID.randomUUID();
+    }
+
+
+    public UUID getUserID(){
+        return this.ID;
+    }
+
+    public String getUserName(){
+        return this.name;
+    }
+
+    public String getUserSurname(){
+        return this.surname;
+    }
+
+    public String getUserTown(){
+        return this.town;
+    }
+
+    public int getUserEmail(){
+        return this.email_address;
+    }
+
