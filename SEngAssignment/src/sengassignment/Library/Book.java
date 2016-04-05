e612349f1c1209be4cba7937ca7275ee2864d948
Caroline Caruana <Caroline Caruana>
2016-04-05 10:29:20 +0200

    Version 2 of constructors and getters

--- SEngAssignment/src/sengassignment/Library/Book.java
+++ SEngAssignment/src/sengassignment/Library/Book.java
@@ -1,15 +1,55 @@
 package sengassignment.Library;
 
 import java.io.*;
+import java.util.UUID;
 
-package sengassignment.Library;
-
-/**
- *
- * @author Caroline
- */
 public class Book {
+    
+    private String title;
+    private String author; 
+    private String genre;
+    private int year;
+    private int edition;  
+    private UUID ID; 
+    
+    
     public Book(){
-        string 
+        title = "";
+        author = ""; 
+        genre = "";
+        year = 0;
+        edition = 0;  
+    }
+    
+    public Book(String title, String author, String genre, int year, int edition){
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.edition = edition;
+        this.ID = UUID.randomUUID();
+    }
+    
+    public UUID getBookID(){
+        return this.ID;
+    }
+    
+    public String getBookTitle(){
+        return this.title;
+    }
+     
+    public String getBookAuthor(){
+        return this.author;
+    }
+    
+      public String getBookGenre(){
+        return this.genre;
+    }
+      
+    public int getBookYear(){
+        return this.year;
+    }
+
+    public int getBookEdition(){
+        return this.edition;
     }
 }
