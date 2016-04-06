a0bf18d4c3598031c2a22a1bafc61bfdd256a526
Caroline Caruana <Caroline Caruana>
2016-04-06 12:07:00 +0200

    Added tests for searchByGenre and searchByTitle. Tests were expected to pass but failed for unknown reason.

--- src/test/java/Tests/CatalogueTest.java
+++ src/test/java/Tests/CatalogueTest.java
@@ -1,6 +1,7 @@
 package Tests;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Assert;
@@ -54,4 +55,51 @@ public class CatalogueTest {
         
         Assert.assertEquals(book_names,c.getAllBooks());
     }
+    
+    @Test
+    public void testSearchByTitle() {
+        Book b1 = new Book("ABC", "AUTHOR", "FICTION", 1948, 7);
+        Book b2 = new Book("ABCD", "AUTHOR", "FANTASY", 1959, 7);
+        Book b3 = new Book("DEF", "AUTHOR", "FICTION", 2000, 7);
+        Book b4 = new Book("EF", "AUTHOR", "ACTION", 1948, 7);
+
+        ArrayList <Book> book_names = new ArrayList<>();
+
+        book_names.add(b1);
+        book_names.add(b2);
+        book_names.add(b3);
+        book_names.add(b4);
+                
+        ArrayList<Book> temp = new ArrayList<>();
+        
+        temp.add(b1);
+        temp.add(b2);
+    
+        Assert.assertEquals(Arrays.asList("ABC", "ABCD"), c.searchByTitle("ABC").toArray());
+    }
+    
+    
+    @Test
+    public void testSearchByGenre(){
+        Book b1 = new Book("ABC", "AUTHOR", "FICTION", 1948, 7);
+        Book b2 = new Book("ABCD", "AUTHOR", "FANTASY", 1959, 7);
+        Book b3 = new Book("DEF", "AUTHOR", "FICTION", 2000, 7);
+        Book b4 = new Book("EF", "AUTHOR", "ACTION", 1948, 7);
+
+        ArrayList <Book> book_names = new ArrayList<>();
+
+        book_names.add(b1);
+        book_names.add(b2);
+        book_names.add(b3);
+        book_names.add(b4);
+        
+        ArrayList<Book> temp = new ArrayList<>();
+        
+        temp.add(b1);
+        temp.add(b3);
+        
+        
+        Assert.assertEquals(temp, c.searchByGenre("Fiction"));
+    }
+    
  }
