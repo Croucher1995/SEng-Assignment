7c63552bd7f96a55d65fcb6659b81ce4154b10a1
Andre Croucher <andrecroucher@gmail.com>
2016-04-06 12:09:04 +0200

    Created search by Year test (Catalogue class)

--- src/test/java/Tests/CatalogueTest.java
+++ src/test/java/Tests/CatalogueTest.java
@@ -102,4 +102,28 @@ public class CatalogueTest {
         Assert.assertEquals(temp, c.searchByGenre("Fiction"));
     }
     
+    @Test
+    public void testSearchByYear(){
+        Book b1 = new Book("ABC", "AUTHOR", "FICTION", 1948, 7);
+        Book b2 = new Book("ABCD", "AUTHOR", "FANTASY", 1959, 7);
+        Book b3 = new Book("DEF", "AUTHOR", "FICTION", 2000, 7);
+        Book b4 = new Book("EF", "AUTHOR", "ACTION", 1948, 7);
+        
+        ArrayList <Book> book_names = new ArrayList<>();
+        
+        book_names.add(b1);
+        book_names.add(b2);
+        book_names.add(b3);
+        book_names.add(b4);
+        
+        ArrayList<Book> temp = new ArrayList<>();
+        
+        temp.add(b1);
+        temp.add(b4);
+        
+        
+        Assert.assertEquals(temp, c.searchBYearOfPublication(1948));
+    }
+    
+    
  }
