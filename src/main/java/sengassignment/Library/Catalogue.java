ad038601326c7dfac131e840b6f4a10aeecda9ab
Andre Croucher <andrecroucher@gmail.com>
2016-04-06 10:18:56 +0200

    Created Catalogue Class with methods

--- src/main/java/sengassignment/Library/Catalogue.java
+++ src/main/java/sengassignment/Library/Catalogue.java
@@ -4,11 +4,65 @@
  * and open the template in the editor.
  */
 package sengassignment.Library;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Arrays;
 
 /**
  *
  * @author Caroline
  */
 public class Catalogue {
+    
+    Book b = new Book ();
+    
+    ArrayList <b> book_names = new ArrayList<b>();
+    
+    
+    public Catalogue() {
+        
+    }
+    
+    public Catalogue(Book b)
+    {
+        book_names.add(b);
+    }
+    
+    public String getAllBooks() {
+        for (int i=0; i< book_names.size(); i++)
+        {
+            System.out.println(book_names.get(i));
+        }
+    }
+    
+    public String searchByTitle(String names){
+        for (int i=0; i< book_names.size(); i++)
+        {
+            if (book_names.contains(names))
+            {
+                return names;
+            }
+        }
+    }
+    
+    public String searchByGenre(String genre){
+        for (int i=0; i< book_names.size(); i++)
+        {
+            if (book_names.contains(genre))
+            {
+                return genre;
+            }
+        }
+    }
+    
+    public int searchByYearOfPublication(int year){
+        for (int i=0; i< book_names.size(); i++)
+        {
+            if (book_names.contains(year))
+            {
+                return year;
+            }
+        }
+    }
     
 }
