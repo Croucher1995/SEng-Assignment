d0e787fccfdccc12c86dde9a595c1d4268a454b2
Caroline Caruana <Caroline Caruana>
2016-04-06 10:59:24 +0200

    Modified Catalogue class to prepare for testing. (Just added 4 dummy books in constructor) Created first test to test getAllBooks. Test expected to pass but failed.

--- src/main/java/sengassignment/Library/Catalogue.java
+++ src/main/java/sengassignment/Library/Catalogue.java
@@ -3,17 +3,22 @@ import java.util.ArrayList;
 
 public class Catalogue {
     
-    Book b = new Book ();
+    Book b1 = new Book("ABC", "AUTHOR", "FICTION", 1948, 7);
+    Book b2 = new Book("ABCD", "AUTHOR", "FANTASY", 1959, 7);
+    Book b3 = new Book("DEF", "AUTHOR", "FICTION", 2000, 7);
+    Book b4 = new Book("EF", "AUTHOR", "ACTION", 1948, 7);
     
     ArrayList <Book> book_names = new ArrayList<>();
     
-    
     public Catalogue() {
-        
+        book_names.add(b1);
+        book_names.add(b2);
+        book_names.add(b3);
+        book_names.add(b4);
     }
     
     public Catalogue(Book b)
-    {
+    {   
         book_names.add(b);
     }
     
--- src/test/java/Tests/CatalogueTest.java
+++ src/test/java/Tests/CatalogueTest.java
@@ -1,20 +1,29 @@
 package Tests;
 
+import java.util.ArrayList;
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import sengassignment.Library.Book;
+import sengassignment.Library.Catalogue;
 
 
 public class CatalogueTest {
+    
+    Catalogue c = new Catalogue();
+    
     
     public CatalogueTest() {
     }
     
     @BeforeClass
     public static void setUpClass() {
+            
+       
     }
     
     @AfterClass
@@ -28,4 +37,21 @@ public class CatalogueTest {
     @After
     public void tearDown() {
     }
-}
+    
+    @Test
+    public void testGetAllBooks() {
+        Book b1 = new Book("ABC", "AUTHOR", "FICTION", 1948, 7);
+        Book b2 = new Book("ABCD", "AUTHOR", "FANTASY", 1959, 7);
+        Book b3 = new Book("DEF", "AUTHOR", "FICTION", 2000, 7);
+        Book b4 = new Book("EF", "AUTHOR", "ACTION", 1948, 7);
+
+        ArrayList <Book> book_names = new ArrayList<>();
+
+        book_names.add(b1);
+        book_names.add(b2);
+        book_names.add(b3);
+        book_names.add(b4);
+        
+        Assert.assertEquals(book_names,c.getAllBooks());
+    }
+ }
