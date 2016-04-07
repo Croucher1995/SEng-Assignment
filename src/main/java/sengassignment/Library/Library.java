1bbec8cd00174511bc2caa77e4559fd899a2b2d2
Andre Croucher <andrecroucher@gmail.com>
2016-04-07 14:48:20 +0200

    Made Changes to the following Classes : 1. Book : Changed id to static int (count) & changed exceeds4weeks method 2. Minor change in Catalogue class 3. Created numberofUsers method, changed loanbookto method 4. Changed ID to count

--- src/main/java/sengassignment/Library/Library.java
+++ src/main/java/sengassignment/Library/Library.java
@@ -1,25 +1,50 @@
 package sengassignment.Library;
+import java.util.ArrayList;
 
 public class Library {
+    ArrayList <User> users = new ArrayList<>();
     
     public Library(){
         
     }
     
     public void addUser(User u){
-        
+        users.add(u);
     }
     
     public void removeUser(User u){
-        
+        users.remove(u);
+    }
+    
+    public int numberofUsers(){
+    	return users.size();
     }
     
     public void loanBookTo(Book b, User u){
+        if(u.loanCounter_check()){ //check if user has exceeded 3 books
+            if(b.isBorrowed()){ //check if book is already on loan
+                if (b.exceeds4Weeks()){// to check if it is overdue
+                    System.out.println("Book already on loan.");
+               } 
         
+                else
+                    System.out.println("Exceeded 4 weeks");
+            }
+            else{
+                b.borrowed();
+                b.loanInfo(u);
+                u.loanCounter_inc();
+                System.out.println("Successful");
+            }       
+        }
+        else
+           System.out.println("User has exceeded maximum number of books.");
+
     }
     
     public void returnBook(Book b){
-        
-    }
-    
+        b.returned();
+        User u = b.getLoanInfo();
+        u.loanCounter_dec();
+    }  
 }
