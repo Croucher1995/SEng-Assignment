54eb018734a59319fb3e04dfa8ab492a630d7488
Caroline Caruana <Caroline Caruana>
2016-04-25 10:02:07 +0200

    Decided to use Composite instead of Facade. Rewrote Filter class to include constructor, accessors and mutators.

--- src/main/java/sengassignment/Library/Filters/Filter.java
+++ src/main/java/sengassignment/Library/Filters/Filter.java
@@ -2,22 +2,50 @@ package sengassignment.Library.Filters;
 import sengassignment.Library.Genre;
 import static sengassignment.Library.Genre.*;
 
-public interface Filter {
-    static String ftitle = "";
-    static String fauthor = ""; 
-    static Genre fgenre = Default;
-    static int fyear = 0;
-    static int fedition= 0;  
+public class Filter {
+    private String ftitle;
+    private String fauthor; 
+    private Genre fgenre;
+    private int fyear;
+    private int fedition  ;
     
-    void setFTitle(String ftitle);
-    void setFAuthor(String fauthor);
-    void setFGenre(Genre fgenre);
-    void setFYear(int fyear);
-    void setFEdition(int fedition);
+    public Filter(){
+        ftitle = "";
+        fauthor = ""; 
+        fgenre = Default;
+        fyear = 0;
+        fedition= 0;  
+    }
     
-    String getFTitle();
-    String getFAutor();
-    String getFGenre();
-    String getFYear();
-    String getFEdition();
+    void setFTitle(String ftitle){
+        this.ftitle = ftitle; 
+    }
+    void setFAuthor(String fauthor){
+        this.fauthor = fauthor; 
+    }
+    void setFGenre(Genre fgenre){
+        this.fgenre = fgenre; 
+    }
+    void setFYear(int fyear){
+        this.fyear = fyear; 
+    }
+    void setFEdition(int fedition){
+        this.fedition = fedition; 
+    }
+    
+    String getFTitle(){
+        return this.ftitle;
+    }
+    String getFAutor(){
+        return this.fauthor;
+    }
+    Genre getFGenre(){
+        return this.fgenre;
+    }
+    int getFYear(){
+        return this.fyear;
+    }
+    int getFEdition(){
+        return this.fedition;
+    }
 }
