b879d5025a15afab56758935b487296e093197a0
Caroline Caruana <Caroline Caruana>
2016-04-25 09:50:15 +0200

    Decided to use Facade for Q2. Created interface Filter.

--- src/main/java/sengassignment/Library/Filter.java
+++ src/main/java/sengassignment/Library/Filter.java
@@ -1,5 +1,22 @@
 package sengassignment.Library;
+import static sengassignment.Library.Genre.*;
 
-public class Filter {
+public interface Filter {
+    static String ftitle = "";
+    static String fauthor = ""; 
+    static Genre fgenre = Default;
+    static int fyear = 0;
+    static int fedition= 0;  
     
+    void setFTitle(String ftitle);
+    void setFAuthor(String fauthor);
+    void setFGenre(Genre fgenre);
+    void setFYear(int fyear);
+    void setFEdition(int fedition);
+    
+    String getFTitle();
+    String getFAutor();
+    String getFGenre();
+    String getFYear();
+    String getFEdition();
 }
