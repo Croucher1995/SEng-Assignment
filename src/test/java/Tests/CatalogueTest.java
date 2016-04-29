142600fd878829b9cfff3c16c7e739a4f36d5e95
Caroline Caruana <Caroline Caruana>
2016-04-29 15:50:45 +0200

    Started testing Catalogue search method

--- src/test/java/Tests/CatalogueTest.java
+++ src/test/java/Tests/CatalogueTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 
 import sengassignment.Library.Book;
 import sengassignment.Library.Catalogue;
+import sengassignment.Library.Filters.*;
 import sengassignment.Library.Filters.Filter;
 import static sengassignment.Library.Genre.*;
 
@@ -18,12 +19,20 @@ public class CatalogueTest {
     
     Book b1 = new Book("HARRY POTTER", "J.K. ROWLING", Fiction, 1997, 5);
     Book b2 = new Book("THE KNIFE OF NEVER LETTING GO", "PATRICK NESS", Fantasy, 2000, 7);
-    Book b3 = new Book("LADY MIDNIGHT", "CASSANDRA CLARE", Fiction, 2000, 7);
+    Book b3 = new Book("THE LADY MIDNIGHT", "CASSANDRA CLARE", Fiction, 2000, 7);
     Book b4 = new Book("THE GLASS SWORD", "VICTORIA AVEYARD", Action, 2016, 3);
     
-    Filter f = new Filter();
-    Filter f1 = new Filter();
-    Filter f2 = new Filter();
+    Filter title = new TitleFilter();
+    Filter author = new AuthorFilter();
+    Filter genre = new GenreFilter();
+    Filter year = new YearFilter();
+    Filter edition = new EditionFilter();
+    
+    Filter f1 = new AndFilters(title, "THE",author,"",genre,"Default",year,"0",edition,"0");
+    Filter f2 = new AndFilters(title, "THE",author,"",genre,"Default",year,"2000",edition,"0");
+    Filter f3 = new AndFilters(title, "THE",author,"",genre,"Fantasy",year,"0",edition,"0");
+    
+    Filter f4 = new OrFilters(title, "THE",author,"",genre,"Default",year,"1997",edition,"0");
 
     public CatalogueTest() {}
     
@@ -39,12 +48,6 @@ public class CatalogueTest {
         c.addBooks(b2);
         c.addBooks(b3);
         c.addBooks(b4);
-        
-        f1.setFTitle("THE");
-        f2.setFYear(2000);
-        f.add(f1);
-        f.add(f2);
-       
     }
     
     @After
@@ -53,5 +56,10 @@ public class CatalogueTest {
     @Test
     public void testGetAllBooks() { 
         assertTrue(c.getAllBooks().contains(b1)&&c.getAllBooks().contains(b2)&&c.getAllBooks().contains(b3)&&c.getAllBooks().contains(b4));  
-    }    
+    }
+    
+    @Test
+    public void testFilter1(){
+        assertTrue(c.searchForBooks(f2).size()==2);
+    } 
 }
